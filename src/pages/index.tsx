eaada7d1eb02286d6867860faa5fcfb734ae4e09
Gabe Hamilton <gabe@near.org>
2024-06-18 14:29:01 -0600

    release: new site

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,18 @@
 import { MetaTags } from '@/components/MetaTags';
 import { useDefaultLayout } from '@/hooks/useLayout';
 import type { NextPageWithLayout } from '@/utils/types';
-import { NearAIPlaceholder } from '@/components/pages/NearAI/NearAIPlaceholder';
+import { NearAIHome } from '@/components/pages/NearAI/NearAIHome';
 
 const HomePage: NextPageWithLayout = () => {
   return (
     <>
       <MetaTags title="NEAR AI" description="NEAR AI: Digital self-sovereignty for the creator economy." />
 
-      <NearAIPlaceholder />
+      <NearAIHome />
     </>
   );
 };
 
+HomePage.getLayout = useDefaultLayout;
 
 export default HomePage;
