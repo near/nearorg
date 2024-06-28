0147f20398638df20a877fd324c37598a4eeee82
Dmitriy <sheleg.dmitriy@gmail.com>
2024-06-28 19:57:37 +0200

    feat: add hzn calendar events to `ai` page

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,3 +2,4 @@ export const CREATE_ACCOUNT_URL = '/signup';
 export const NEAR_AI_URL = 'https://near.ai';
 export const LUMA_NEAR_CALENDAR_ID = 'cal-Nrz4EsmLDjXvjPp';
 export const LUMA_NEAR_AI_CALENDAR_ID = 'cal-dzvTEx2EC4F2tk8';
+export const LUMA_NEAR_HZN_CALENDAR_ID = 'cal-BOILOqC74o135WB';
