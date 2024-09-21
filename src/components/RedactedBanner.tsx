063b11ea26aa28af467d81f5c529c8ed4208927c
Caleb Jacob <thecalebjacob@gmail.com>
2024-09-21 11:39:20 -0600

    Add dynamic redacted banner logic to show/hide

--- src/components/RedactedBanner.tsx
+++ src/components/RedactedBanner.tsx
@@ -3,6 +3,14 @@ import { Container } from '@near-pagoda/ui';
 import s from './RedactedBanner.module.scss';
 
 export const RedactedBanner = () => {
+  const now = Date.now();
+  const redactedEndTime = 1731139200000; // Nov 9th 2024
+  const hackathonEndTime = 1727247600000; // Oct 25th 2024
+  const hackathonEndPassed = now > hackathonEndTime;
+  const redactedEndPassed = now > redactedEndTime;
+
+  if (redactedEndPassed) return null;
+
   return (
     <Container>
       <div className={s.redactedBanner}>
@@ -13,12 +21,16 @@ export const RedactedBanner = () => {
           />
         </a>
 
-        <a href="https://redacted.devpost.com/" target="_blank">
-          <img
-            src="/images/redacted/redacted-right.png"
-            alt="Redacted: It's so much more than a hackathon. Click to learn more"
-          />
-        </a>
+        {hackathonEndPassed ? (
+          <img src="/images/redacted/redacted-right-empty.png" alt="Redacted" />
+        ) : (
+          <a href="https://redacted.devpost.com/" target="_blank">
+            <img
+              src="/images/redacted/redacted-right.png"
+              alt="Redacted: It's so much more than a hackathon. Click to learn more"
+            />
+          </a>
+        )}
       </div>
     </Container>
   );
